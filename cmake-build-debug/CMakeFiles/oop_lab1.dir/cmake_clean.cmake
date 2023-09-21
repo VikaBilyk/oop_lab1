ab3eae636e5989bc95ed086cca14c4c90e66daf5
viktoriyabilyk <vikabilyk3@gmail.com>
2023-09-21 20:49:30 +0300

    create new VCS

---
+++ cmake-build-debug/CMakeFiles/oop_lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_lab1.dir/main.cpp.o"
+  "oop_lab1"
+  "oop_lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
