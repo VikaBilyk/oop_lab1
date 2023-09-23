4e44a6d5d76d8c1be7a62c804bd4edc0578525f9
viktoriyabilyk <vikabilyk3@gmail.com>
2023-09-24 00:00:40 +0300

    create main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,26 @@
 // Created by Вікторія Білик on 21.09.2023.
 //
 
-#include "iostream"
+#include <iostream>
+
 
 using namespace std;
 
+
+bool is_data_correct(int year, int month, int day){
+
+    if(year < 1 or month < 1 or month > 12 or day < 1)
+        return false;
+
+    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if(year % 400 == 0 and (year % 100 != 0 or year % 4 == 0))
+        days_in_month[1] = 29; // типу високосний
+        return day <= days_in_month[month];
+
+}
 int main()
 {
-    int x;
-    cin>>x;
-    cout<<x<<'\n';
-    cout<<"lol\n";
+
+cout << is_data_correct(2020, 4, 6);
     return 0;
 }
